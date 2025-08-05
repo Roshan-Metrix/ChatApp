0c944d828ac90ba30650edebb01d4212d6b4b14f
Roshan Patel <roshanpatel12309@gmail.com>
2025-08-05 14:33:56 +0545

    Decode token and get userId

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import userAuth from '../middleware/userAuth.js'
+import { isAuthenticated, loginUser, logoutUser, registerUser, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+
 
 const authRouter = express.Router();
 
 authRouter.post('/signup',registerUser)
 authRouter.post('/login',loginUser)
 authRouter.post('/logout',logoutUser)
+authRouter.post('/send-verify-otp', userAuth ,sendVerifyOtp)
+authRouter.post('/verify-email',verifyEmail)
+authRouter.post('/is-auth',isAuthenticated)
+authRouter.post('/send-reset-otp',sendResetOtp)
+authRouter.post('/reset-password',resetPassword)
 
 
 export default authRouter;
